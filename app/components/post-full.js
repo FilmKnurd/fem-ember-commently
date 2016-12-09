12e5643e92e22fead8a936c667d440f951abb7f3
FilmKnurd <andrew@andrewelster.com>
2016-12-09 12:14:26 -0500

    Draft state

--- app/components/post-full.js
+++ app/components/post-full.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { Component } = Ember;
 
 export default Component.extend({
-    classNames: ['post-full']
+    classNames: ['post-full'],
+    draft: stateFor('post-info', 'model'),
+
+    actions: {
+        postComment() {
+            // let post = this.get('model');
+            // post.get('comments').push(this.get('draft.comment'));
+            // post.save();
+        }
+    }
 });
--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
+
 
 const { Component } = Ember;
 
 export default Component.extend({
-    classNames: ['post-tile']
+    classNames: ['post-tile'],
+    classNameBindings: ['hasDraft'],
+    draft: stateFor('post-info', 'model'),
+
+    hasDraft: Ember.computed.bool('draft.comment')
 });
